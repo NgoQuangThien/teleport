60f1f73610eb2a9145cff3e137dbe3331e9bb82d
Ngo Thien <quangthien18032000@gmail.com>
2022-08-20 12:30:31 +0700

    Upload

---
+++ setup.py
@@ -0,0 +1,54 @@
+import os
+import yaml
+
+
+teleport_labels = '/etc/teleport.labels'
+teleport_config = '/etc/teleport.yaml'
+teleport_exec = '/usr/bin/teleport-labels.py'
+
+IP = yaml.safe_load('''
+- name: ip
+  command: [hostname, -I]
+  period: 1m0s''')
+
+UPTIME = yaml.safe_load('''
+- name: ip
+  command: [uptime, -p]
+  period: 1m0s''')
+
+OTHER = yaml.safe_load('''
+- name: group
+  command: [/usr/bin/python3, /usr/bin/teleport-labels.py, groups]
+  period: 1m0s
+
+- name: owner
+  command: [/usr/bin/python3, /usr/bin/teleport-labels.py, owner]
+  period: 1m0s
+
+- name: other
+  command: [/usr/bin/python3, /usr/bin/teleport-labels.py, other]
+  period: 1m0s''')
+
+
+def read_yaml(path):
+    with open(path) as f:
+        yaml_content = os.path.expandvars(f.read())
+        return yaml.safe_load(yaml_content)
+
+
+def write_file(file_path, content):
+    with open(file_path, 'w') as f:
+        f.write(content)
+    return True
+
+
+if __name__ == '__main__':
+    os.system('cp -f teleport.labels {0}'.format(teleport_labels))
+    os.system('cp -f teleport-labels.py {0}'.format(teleport_exec))
+
+    config = read_yaml(teleport_config)
+    config['ssh_service']['commands'].extend(IP)
+    config['ssh_service']['commands'].extend(UPTIME)
+    config['ssh_service']['commands'].extend(OTHER)
+
+    write_file(teleport_config, yaml.safe_dump(config))
---
+++ teleport-labels.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import yaml
+
+
+file_path = '/etc/teleport.labels'
+
+
+def read_yaml(path):
+    with open(path) as f:
+        yaml_content = os.path.expandvars(f.read())
+        return yaml.load(yaml_content)
+
+
+if __name__ == '__main__':
+    labels = read_yaml(file_path)
+    key = sys.argv[1]
+    result = str()
+    if key in labels:
+        result = str(labels[key]).replace("'", "")
+        result = result.replace("[", "")
+        result = result.replace("]", "")
+    print(result)
