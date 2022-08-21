f60c7622054494c951a776f8fcfe69274bead52f
Ngo Thien <quangthien18032000@gmail.com>
2022-08-21 19:43:23 +0700

    Upload

--- teleport-labels.py
+++ teleport-labels.py
@@ -9,7 +9,7 @@ file_path = '/etc/teleport.labels'
 def read_yaml(path):
     with open(path) as f:
         yaml_content = os.path.expandvars(f.read())
-        return yaml.load(yaml_content)
+        return yaml.safe_load(yaml_content)
 
 
 if __name__ == '__main__':
